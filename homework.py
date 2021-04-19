677ac32c93e37b12d2a2000253fcf07dfdf48ad3
Назарова Екатерина <nazarovaea1@gmail.com>
2021-04-19 11:39:10 +0300

    After second revue

--- homework.py
+++ homework.py
@@ -18,35 +18,33 @@ load_dotenv()
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+HOMEWORK_STATUS = {
+    'approved': ('Ревьюеру всё понравилось, можно приступать к следующему '
+                 'уроку.'),
+    'rejected': 'К сожалению в работе нашлись ошибки.',
+}
+PRAK_HOMEWORK_API = ('https://praktikum.yandex.ru/api/user_api/'
+                     'homework_statuses/')
 
 
 def parse_homework_status(homework):
     homework_name = homework.get('homework_name')
     homework_status = homework.get('status')
-    homework_statuses = {
-        'approved': ('Ревьюеру всё понравилось, можно приступать к следующему '
-                     'уроку.'),
-        'rejected': 'К сожалению в работе нашлись ошибки.',
-    }
-
-    if homework_status not in homework_statuses:
+    if homework_status not in HOMEWORK_STATUS:
         return 'Статус неизвестен'
-    verdict = homework_statuses[homework_status]
+    verdict = HOMEWORK_STATUS[homework_status]
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
 def get_homework_statuses(current_timestamp):
-    prak_homework_api = ('https://praktikum.yandex.ru/api/user_api/'
-                         'homework_statuses/')
-    homework_statuses = requests.get(
-        prak_homework_api,
-        headers={'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'},
-        params={'from_date': current_timestamp, })
-    return homework_statuses.json()
     try:
-        homework_statuses.raise_for_status()
-    except requests.exceptions.HTTPError as err:
-        raise SystemExit(err)
+        homework_statuses = requests.get(
+            PRAK_HOMEWORK_API,
+            headers={'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'},
+            params={'from_date': current_timestamp, })
+        return homework_statuses.json()
+    except requests.exceptions.RequestException:
+        return {}
 
 
 def send_message(message, bot_client):
@@ -57,25 +55,22 @@ def send_message(message, bot_client):
 def main():
     bot_client = Bot(token=TELEGRAM_TOKEN)
     logging.debug('Бот запущен')
-
     while True:
         try:
             current_timestamp = int(time.time())
             new_homework = get_homework_statuses(current_timestamp)
+            hw = new_homework.get('homeworks', {})
             if new_homework.get('homeworks'):
-                if (len(new_homework.get(
-                    'homeworks')[0]) > 0 and new_homework.get(
-                        'homeworks')[0] is not None):
-                    send_message(parse_homework_status(
-                        new_homework.get('homeworks')[0]), bot_client)
+                if (len(hw)[0]) > 0:
+                    send_message(parse_homework_status(hw[0]), bot_client)
                 else:
                     send_message('Домашек для проверки нет', bot_client)
             current_timestamp = new_homework.get(
                 'current_date', current_timestamp)
-            time.sleep(300)
+            time.sleep(10)
 
         except Exception as e:
-            time.sleep(5)
+            time.sleep(300)
             logging.error(f'Бот столкнулся с ошибкой: {e}')
 
 
